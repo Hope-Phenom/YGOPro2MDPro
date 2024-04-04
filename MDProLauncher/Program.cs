3686b7e671e9746a1932801f627dc7d2b04f0e4e
Hope.Phenom <hope.phenom@foxmail.com>
2024-04-04 20:22:53 +0800

    添加项目文件。

---
+++ MDProLauncher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MDProLauncher
+{
+    internal class Program
+    {
+        private const string settingPath = "launcher.cfg";
+
+        static void Main(string[] args)
+        {
+            var argsLine = string.Join(" ", args.ToArray());
+            var path = File.ReadAllText(settingPath);
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = path,
+                    Arguments = argsLine,
+                    UseShellExecute = true,
+                    CreateNoWindow = false,
+                    WorkingDirectory = Path.GetDirectoryName(path)
+                }
+            };
+            File.WriteAllText("lines.cfg", argsLine);
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
